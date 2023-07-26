94536ed6ad42d4855b17b463aba0c6e5bf5c5f25
teradaian <teradaian@gmail.com>
2023-07-26 07:30:14 -0700

    import browserrouter, weap app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Form from './components/Form/Form' 
+import Default from './pages/Default/Default'
 
 function App() {
 
-
   return (
-    <>
-    </>
+    <Default />
+    <Form />
   )
 }
 
