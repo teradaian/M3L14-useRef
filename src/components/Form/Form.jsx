034b4b17d9f77ff82beba533ce5907bed94c9c91
teradaian <teradaian@gmail.com>
2023-07-26 08:06:40 -0700

    add styling, fix input color

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,59 @@
+import './Form.css'
+
 const Form = () => {
-  return ( <></> );
+  return (
+    <main className="new">
+      <h1>NEW</h1>
+      <form autoComplete="off">
+        <label htmlFor="name-input">Name</label>
+        <input
+          required
+          type="text"
+          name="name"
+          id="name-input"
+          placeholder="Name"
+        />
+
+        <label htmlFor="title-input">Age</label>
+        <input
+          required
+          min="0"
+          type="number"
+          name="age"
+          id="age-input"
+          placeholder="Age"
+        />
+
+        <label htmlFor="breed-input">Breed</label>
+        <input
+          required
+          type="text"
+          name="breed"
+          id="breed-input"
+          placeholder="Breed"
+        />
+
+        <label htmlFor="color-input">Color</label>
+        <select required name="color" id="color-input">
+          <option value="Grey">Grey</option>
+          <option value="Black">Black</option>
+          <option value="Brown">Brown</option>
+          <option value="White">White</option>
+          <option value="Golden">Golden</option>
+        </select>
+
+        <label htmlFor="ears-input">Ears</label>
+        <select required name="ears" id="ears-input">
+          <option value="Drop">Drop</option>
+          <option value="Pointy">Pointy</option>
+          <option value="Button">Button</option>
+        </select>
+
+        <button type="submit">SUBMIT</button>
+
+      </form>
+    </main>
+  )
 }
  
 export default Form;
