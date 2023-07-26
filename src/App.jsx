f96cbbf399baf10043c12e9a70920c1d7895b5d2
teradaian <teradaian@gmail.com>
2023-07-26 09:15:34 -0700

    baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+
 import Nav from './components/Nav/Nav'
 import SignUpForm from './components/Form/SignUpForm' 
 import Landing from './pages/Landing/Landing'
