0cf6fcb51ef4b04c29ee5af2947a3104d1672c8f
teradaian <teradaian@gmail.com>
2023-07-26 09:02:54 -0700

    convert userslist map

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,14 @@
 const Landing = ({ users }) => {
+
+  const usersList = users.map(user => (
+    <h1 key={user.username}>
+      {user.username}
+    </h1>
+  ))
+
   return (
     <div>
-      { users.map(user => <h1 key={user.username}>{user.username}</h1>) }
+      { usersList }
     </div>
    );
 }
